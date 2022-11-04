a659e3997c755e656faad32d41013bb449cf264b
Nikhsaylu <ulyashkinm13@gmail.com>
2022-11-05 01:21:43 +0300

    ДЗ

---
+++ src/Main.java
@@ -0,0 +1,104 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        var dog = 8;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+
+
+
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+        var dog = 8;
+        dog = dog + 4;
+        System.out.println(dog);
+        var cat = 3.6;
+        cat = cat + 4;
+        System.out.println(cat);
+        var paper = 763789;
+        paper = paper + 4;
+        System.out.println(paper);
+
+
+
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        var dog = 4.5;
+        System.out.println(dog);
+        var cat = 3.6;
+        cat = cat - 1.6;
+        System.out.println(cat);
+        var paper = 763789;
+        paper = paper - 7639;
+        System.out.println(paper);
+    }
+    public static void task4 () {
+            System.out.println("Задача 4");
+            var friend = 19;
+            System.out.println(friend);
+            friend = friend * 2;
+            System.out.println(friend);
+            friend = friend / 7;
+            System.out.println(friend);
+    }
+    public static void task5() {
+        System.out.println("Задача 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+    }
+    public static void task6() {
+        System.out.println("Задача 6");
+        var firstBoxerWeight = 78.2;
+        var secondBoxerWeight = 82.7;
+        var totalWeight = firstBoxerWeight + secondBoxerWeight;
+        System.out.println("Общий вес боксеров " + totalWeight);
+    }
+    public static void task7() {
+        System.out.println("Задача 7");
+        var firstBoxerWeight = 78.2;
+        var secondBoxerWeight = 82.7;
+        var totalWeight = firstBoxerWeight + secondBoxerWeight;
+        System.out.println("Общий вес боксеров " + totalWeight + " кг");
+        var differenceWeignt = secondBoxerWeight - firstBoxerWeight;
+        System.out.println("Разница в весе " +differenceWeignt + " кг");
+        var procent = secondBoxerWeight % firstBoxerWeight;
+        System.out.println("Остаток от деления " + procent + " процентов");
+    }
+    public static void task8() {
+        System.out.println("Задача 8");
+        var totalHours = 640;
+        var worker = 8;
+        var totalWorker = totalHours / worker;
+        System.out.println("Всего работников в компании " + totalWorker);
+        var plusWorkers = 94;
+        var plusTotalWorkers = totalWorker + plusWorkers;
+        var hoursForPlus = totalHours / plusTotalWorkers;
+        System.out.println("Если в компании работает " + plusTotalWorkers + " человек," + "То всего "+hoursForPlus + " часов работы может быть поделено между сотрудниками");
+
+    }
+
+}
